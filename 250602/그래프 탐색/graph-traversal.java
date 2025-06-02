b21e675909474f358f033e36d497ade834320ef7
JinhyeonK <diamondataJH@gmail.com>
2025-06-02 16:13:50 +0900

    Title: 그래프 탐색, Time: 428ms, Memory: 23MB, Status: Passed - Codetree

--- 250602/그래프 탐색/graph-traversal.java
+++ 250602/그래프 탐색/graph-traversal.java
@@ -1,34 +1,50 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static final int MAX_NUM=1000;
-    public static int n,m;
-    public static int[][] graph =new int[MAX_NUM+1][MAX_NUM+1];
-    public static boolean[] visited =new boolean[MAX_NUM+1];
+    public static final int MAX_NUM = 1000;
+    public static int n, m;
+
+
+    // index를 1번 부터 사용하기 위해 MAX_NUM+1만큼 할당합니다.
+    public static ArrayList<Integer>[] graph = new ArrayList[MAX_NUM + 1];
+    public static boolean[] visited = new boolean[MAX_NUM + 1];
     public static int vertexCnt = 0;
-    public static void dfs(int vertex){
-        for(int currV =1; currV<=n; currV++){
-            if(graph[vertex][currV]==1 && !visited[currV]){
-                visited[currV]=true;
+
+    public static void DFS(int vertex) {
+        // 해당 정점에서 이어져있는 모든 정점을 탐색해줍니다.
+        for(int i = 0; i < graph[vertex].size(); i++) {
+            int currV = graph[vertex].get(i);
+            if(!visited[currV]) {
+                // 아직 방문한 적이 없는 정점에 대해서만 탐색을 진행합니다.
+                visited[currV] = true;
                 vertexCnt++;
-                dfs(currV);
+                DFS(currV);
             }
         }
     }
-    public static void main(String[] args) throws IOException{
-        // Please write your code here.
-    BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-    n=Integer.parseInt(br.readLine());
-    m=Integer.parseInt(br.readLine());
 
-    for (int i = 0; i < m; i++) {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
-        graph[v1][v2] = 1;
-        graph[v2][v1] = 1;
-    visited[1]=true;
-    dfs(1);
-    System.out.println(vertexCnt);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for(int i = 1; i <= n; i++)
+            graph[i] = new ArrayList<>();
+
+        while(m-- > 0) {
+            // 각 정점이 서로 이동이 가능한 양방향 그래프이기 때문에
+            // 각 정점에 대한 간선을 각각 저장해줍니다.
+            int v1 = sc.nextInt();
+            int v2 = sc.nextInt();
+
+            graph[v1].add(v2);
+            graph[v2].add(v1);
+        }
+       
+        visited[1] = true;
+        DFS(1);
+
+        System.out.println(vertexCnt);
     }
 }
