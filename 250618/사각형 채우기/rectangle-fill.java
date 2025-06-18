cbace4f4e892694546488e3bda6d398f12376099
JinhyeonK <diamondataJH@gmail.com>
2025-06-18 16:40:06 +0900

    Title: 사각형 채우기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250618/사각형 채우기/rectangle-fill.java
+++ 250618/사각형 채우기/rectangle-fill.java
@@ -1,16 +1,28 @@
-import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        final int MOD = 10007;
-        // Please write your code here.
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int [][]dp=new int [n-1][1];
-        for (int i = 0; i <n; i++) {
-            for(int j=0;j<=1;j++)
-            dp[n][2] = (dp[i-1][j] + dp[i-2][j]) % MOD;
-        }
-        System.out.print(dp[n][2]);
+import java.util.Scanner;
 
+public class Main {
+    public static final int MOD = 10007;
+    public static final int MAX_N = 1000;
+    
+    // 변수 선언
+    public static int n;
+    
+    public static int[] dp = new int[MAX_N + 1];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+
+        // 초기 조건 설정
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // 점화식에 따라 dp값 채우기
+        // dp[i] = dp[i - 1] + dp[i - 2]
+        for(int i = 2; i <= n; i++)
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+
+        System.out.print(dp[n]);
     }
 }
